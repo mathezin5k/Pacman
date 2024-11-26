5a8d70246e36bb719e8588cac71d5931e2ed489c
mathezin5k <142324331+mathezin5k@users.noreply.github.com>
2024-11-26 12:35:44 -0300

    finish Ghosts inky removed

--- pacman/src/pacman/Personagens.java
+++ pacman/src/pacman/Personagens.java
@@ -1,19 +1,17 @@
 package pacman;
 
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 
 public abstract class Personagens {
-    protected int posX, posY;   // Posições do personagem
-    protected int speed;        // Velocidade do personagem
-    protected boolean isSuper;  // Estado especial do personagem
-    public BufferedImage up1, up2, up3, down1, down2, down3, left1, left2, left3, right1, right2, right3, ghost1, ghost2, ghost3, ghost4;
-
+    protected int posX, posY;   
+    protected int speed;        
+    protected boolean isSuper;  
+    
     public Personagens(int startX, int startY, int speed) {
         this.posX = startX;
         this.posY = startY;
         this.speed = speed;
-        this.isSuper = false;  // Estado inicial de isSuper é false
+        this.isSuper = false;
     }
 
     public boolean getIsSuper() {
@@ -24,8 +22,7 @@ public abstract class Personagens {
         this.isSuper = isSuper;
     }
 
-    public abstract void update(KeyHandler keyH,  int pacX, int pacY);
+    public abstract void update(KeyHandler keyH,  int pacX, int pacY, Blinky blinky, Clyde clyde, Pinky pinky);
 
-    // Método abstrato para desenhar o personagem
     public abstract void draw(Graphics2D g2, int blockSize);
 }
